c98fc56ed8e59cd4402dfc0928a6f6f2bedc72ee
kukaro <justkukaro@naver.com>
2024-03-03 10:09:01 +0900

    feat:init project

---
+++ src/main/java/org/example/springmvcexamplenonesecurity/SpringMvcExampleNoneSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.springmvcexamplenonesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcExampleNoneSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcExampleNoneSecurityApplication.class, args);
+    }
+
+}
